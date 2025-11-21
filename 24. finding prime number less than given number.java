6c0fd230de23857265cc8eeec76ac427e3a6a128
Shreeram Yadav <113875685+shreeram2011@users.noreply.github.com>
2025-11-21 11:04:11 +0530

    Add prime number finding program
    Implement a program to find and print all prime numbers less than or equal to a given number.

---
+++ 24. finding prime number less than given number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+    public static boolean isPrime(int n){
+        if(n <= 1) return false;
+        if(n <= 3) return true;
+        
+        if(n % 2 == 0 || n % 3 == 0) return false;
+        for(int i = 5; i * i <= n; i += 6){
+            if(n % i == 0 || n % (i + 2) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i ++){
+		    if(isPrime(i)){
+		        System.out.print(i + " ");
+		    }
+		}
+	}
+}
